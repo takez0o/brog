e10b6924ff63b7912b6c6210f37f135c3298b207
takez0_o <takez0_o@protonmail.com>
2023-05-27 10:05:51 +0300

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+	import { assets } from '$app/paths';
+</script>
+
+<main>
+	<section>
+		<div class="header">
+			<h1>Tornado Cash Governance</h1>
+			<div class="small">Monday, 22 May 23'</div>
+		</div>
+		<p class="divider">
+			--------------------------------------------------------------------------------------------------
+		</p>
+		<div class="article">
+			<p>
+				A reentrancy attack is a type of attack that exploits the connections between smart
+				contracts to drain funds or manipulate the data stored inside the smart contract.A
+				reentrancy attack is a type of attack that exploits the connections between smart contracts
+				to drain funds or manipulate the data stored inside the smart contract. <br /><br />Smart
+				contracts have been notoriously vulnerable to exploits. There hasn't been a period in which
+				no smart contract has not been exploited in the near past. As Decoded Labs, we have decided
+				to start a series of articles explaining these exploits. The first article is about the
+				reentrancy attack, which caused over $100 million to be stolen up until now.
+			</p>
+			<br />
+			<div class="image" />
+			<p>
+				A reentrancy attack is a type of attack that exploits the connections between smart
+				contracts to drain funds or manipulate the data stored inside the smart contract.A
+				reentrancy attack is a type of attack that exploits the connections between smart contracts
+				to drain funds or manipulate the data stored inside the smart contract. <br /><br />
+				Smart contracts have been notoriously vulnerable to exploits. There hasn't been a period in which
+				no smart contract has not been exploited in the near past. As Decoded Labs, we have decided to
+				start a series of articles explaining these exploits. The first article is about the reentrancy
+				attack, which caused over $100 million to be stolen up until now.
+			</p>
+			<p>
+				A reentrancy attack is a type of attack that exploits the connections between smart
+				contracts to drain funds or manipulate the data stored inside the smart contract. A
+				reentrancy attack is a type of attack that exploits the connections between smart contracts
+				to drain funds or manipulate the data stored inside the smart contract. Smart contracts have
+				been notoriously vulnerable to exploits. <br /><br />There hasn't been a period in which no
+				smart contract has not been exploited in the near past. As Decoded Labs, we have decided to
+				start a series of articles explaining these exploits. The first article is about the
+				reentrancy attack, which caused over $100 million to be stolen up until now. Smart contracts
+				have been notoriously vulnerable to exploits. There hasn't been a period in which no smart
+				contract has not been exploited in the near past.
+			</p>
+		</div>
+		<p class="divider">
+			--------------------------------------------------------------------------------------------------
+		</p>
+		<div class="header">
+			<a href="/home" class="small">blog/tornado_cash_governance</a>
+			<a href="https://twitter.com/takez0_o" target="_blank" class="small">written by @takez0_o</a>
+		</div>
+	</section>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.header {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: flex-end;
+	}
+	.divider {
+		line-height: 8px;
+		padding: 4px 0px;
+	}
+	.article {
+		padding: 12px 0px;
+	}
+	p {
+		float: none;
+	}
+</style>
